ff21a0c03b65af1b2524e90a9827588ac759e4d6
Italo Ferreira <grd788@gmail.com>
2021-12-30 22:17:03 -0300

    Atualizações e Modificações JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('teste')
+
+const links = document.querySelectorAll(".header-menu a")
+
+function ativarLink(link){
+  const url = location.href
+  const href = link.href
+
+  if(url.includes(href)){
+    link.classList.add("ativo")
+  }
+}
+
+links.forEach(ativarLink)
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarParametro(parametro){
+  const element = document.getElementById(parametro)
+  if(element){
+    element.checked = true
+  }
+}
+
+parametros.forEach(ativarParametro)
